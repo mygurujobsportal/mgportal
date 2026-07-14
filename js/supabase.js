dca420604808cfcbd730333e795ce724038b3161
mygurujobsportal <myguru.jobsportal@gmail.com>
2026-07-14 12:29:26 +0530

    Update supabase.js

--- js/supabase.js
+++ js/supabase.js
@@ -1,18 +1,15 @@
-/* =========================================
+/* ==========================================
    MyGuru Jobs Portal
    Supabase Configuration
-========================================= */
+========================================== */
 
-// Your Supabase Project URL
 const SUPABASE_URL = "https://sugomkzvytuncybafngf.supabase.co";
 
-// Your Publishable (Anon) Key
 const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InN1Z29ta3p2eXR1bmN5YmFmbmdmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODM1NjM5ODQsImV4cCI6MjA5OTEzOTk4NH0.JNwUYVXdqj0hyVhf1ZjL-YDvY_AekJg9sZE9fuDH9q0";
 
-// Create Global Supabase Client
 window.supabaseClient = window.supabase.createClient(
     SUPABASE_URL,
     SUPABASE_ANON_KEY
 );
 
-console.log("✅ Supabase Connected Successfully");
+console.log("Supabase Connected");
