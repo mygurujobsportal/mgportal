e3c76d51231852b4460a3d3e4369ca29e10afe2b
mygurujobsportal <myguru.jobsportal@gmail.com>
2026-07-14 21:19:42 +0530

    Update supabase.js

--- js/supabase.js
+++ js/supabase.js
@@ -1,6 +1,6 @@
 // MyGuru Central Supabase Client Engine Configuration
 const SUPABASE_URL = "https://iaaetymxqyjrgwrzrimp.supabase.co";
-const SUPABASE_KEY = "sb_publishable_YwOJUt0JVvWlpf01eV5y6Q_t3LYbQqy"; 
+const SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InN1Z29ta3p2eXR1bmN5YmFmbmdmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODM1NjM5ODQsImV4cCI6MjA5OTEzOTk4NH0.JNwUYVXdqj0hyVhf1ZjL-YDvY_AekJg9sZE9fuDH9q0"; 
 
 const _supabase = supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
 
